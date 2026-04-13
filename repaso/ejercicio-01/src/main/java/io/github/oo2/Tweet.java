346cc777f849a9075cd020d7e6388df891256816
agustin firpo <agustin_firpoutlook.com.ar>
2026-04-13 14:14:41 -0300

    actualización: ejercicio 1 - practica repaso - en proceso

--- repaso/ejercicio-01/src/main/java/io/github/oo2/Tweet.java
+++ repaso/ejercicio-01/src/main/java/io/github/oo2/Tweet.java
@@ -21,4 +21,8 @@ public class Tweet implements Publicacion {
     return false;
   }
 
+
+
+
+
 }
--- repaso/ejercicio-01/src/main/java/io/github/oo2/Usuario.java
+++ repaso/ejercicio-01/src/main/java/io/github/oo2/Usuario.java
@@ -12,11 +12,32 @@ public class Usuario {
     this.publicaciones = new ArrayList<Publicacion>();
   }
 
-  public void setScreenName(String nombre){
+  public void setScreenName(String nombre) {
     this.screenName = nombre;
   }
 
   public String getScreenName() {
     return this.screenName;
+  }
+
+  public boolean cumpleLongitud(String texto) {
+    return (texto.length() >= 1 && texto.length() <= 280);
+  }
+
+  public void eliminarPublicaciones() {
+    this.publicaciones.clear();
+  }
+
+  public void publicarTweet(String texto) {
+    Tweet tweet = null;
+    if (this.cumpleLongitud(texto)) {
+      tweet = new Tweet(texto);
+      this.publicaciones.add(tweet);
+    }
+  }
+
+  public void retweetear(Tweet tweet){
+    Retweet retweet = new Retweet(tweet);
+    this.publicaciones.add(retweet);
   }
 }
